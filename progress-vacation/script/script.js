549531d17b426f7ab3889298e431c50ebad99325
Pavel Karachev <pavel.karachev@team.wrike.com>
2019-10-24 14:44:24 +0300

    Добавляет прогресс времени

--- progress-vacation/script/script.js
+++ progress-vacation/script/script.js
@@ -1,5 +1,3 @@
-
-
 function updateTime() {
   let now = new Date();
   let startVacation = new Date(2019, 9, 21, 3, 5);
@@ -8,6 +6,7 @@ function updateTime() {
   let progressFloor =  Math.floor(progress * 100) / 100;
   
   renderProgress(progressFloor);
+  renderTime(now, finishVacation);
 }
 
 function renderProgress(value) {
@@ -16,8 +15,33 @@ function renderProgress(value) {
   queryLine.style.width = `${value}%`;
 }
 
+function renderTime(now, finish) {
+  let queryDays = document.querySelector('.statistic__days');
+  let queryHours = document.querySelector('.statistic__hours');
+  let queryMinutes = document.querySelector('.statistic__minutes');
+  let querySeconds = document.querySelector('.statistic__seconds');
+  
+  queryDays.innerHTML = countsTime(now, finish, 'days');
+  queryHours.innerHTML = countsTime(now, finish, 'hours');
+  queryMinutes.innerHTML = countsTime(now, finish, 'minutes');
+  querySeconds.innerHTML = countsTime(now, finish, 'seconds');
+}
+
+function countsTime(now, finish, type) {
+    switch (type) {
+      case 'days':
+        return Math.floor((finish - now) / (1000 * 60 * 60 * 24));
+      case 'hours':
+        return Math.floor((finish - now) / (1000 * 60 * 60)) % 24;
+      case 'minutes':
+        return Math.floor((finish - now) / (1000 * 60)) % 60;
+      case 'seconds':
+        return Math.floor((finish - now) / (1000)) % 60;
+    }
+}
+
 function init() {
-  setInterval(updateTime, 200);
+  setInterval(updateTime, 1000);
 }
 
 init();
